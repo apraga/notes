dd813f2b119466d94e6af8261d5c459a06f6e5c5
Alexis Praga <apraga@chu-besancon.fr>
2023-01-31 11:38:11 +0100

    Proofreading biochemitry

--- notes/biochimie/Shakefile.hs
+++ notes/biochimie/Shakefile.hs
@@ -3,18 +3,27 @@ import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
 
+gds = [ "bicarbonates"
+      , "calcium"
+      , "chlore"
+      , "pco2"
+      , "ph"
+      , "po2"
+      ]
+
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-    want ["docx"]
+    want ["_build" </> x <.> "docx" | x <- gds ]
 
     phony "clean" $ do
         putInfo "Cleaning files in _build"
         removeFilesAfter "_build" ["//*"]
 
-    phony "docx" $ do
-        org <- getDirectoryFiles "" ["parametres//*.org"]
-        need [ "_build" </> dropDirectory1 (c -<.> "docx") | c <- org ]
+    -- phony "docx" $ do
+    --     org <- getDirectoryFiles "" ["parametres//*.org"]
+    --     need [ "_build" </> dropDirectory1 (c -<.> "docx") | c <- org ]
 
     "_build//*.docx" %> \out -> do
         let c = "parametres" </> dropDirectory1 out -<.> "org"
+        need [c]
         cmd_ "pandoc" [c] "-o" [out]
