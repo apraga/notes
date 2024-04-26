460682f8ccfbf05cf3d7588518283f0b5b6d6ffa
Alexis Praga <alexis.praga@proton.me>
2024-04-26 16:49:16 +0200

    Removed slides into another repo (site)

--- site/static/slides/fosdem2024/assets/channel-d3ac35ac-CX7CKzfx.js
+++
@@ -1,1 +0,0 @@
-import{aS as o,aT as r}from"./slidev/Mermaid-BK6vYFFb.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
--- site/static/slides/fosdem2024/assets/clone-d2e37f8c-CHMHSaUq.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./graph-af3d5df6-CpzLvc9u.js";var e=4;function a(o){return r(o,e)}export{a as c};
